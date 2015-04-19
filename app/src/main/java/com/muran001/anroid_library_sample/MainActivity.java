e502fe9fa6caf6dba4e95344586c6df1ae2e7b2e
muran001 <macmuran001@gmail.com>
2015-04-19 22:05:35 +0900

    add LibraryListView at MainActivity

--- app/src/main/java/com/muran001/anroid_library_sample/MainActivity.java
+++ app/src/main/java/com/muran001/anroid_library_sample/MainActivity.java
@@ -1,40 +1,72 @@
 package com.muran001.anroid_library_sample;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import android_sample.muran001.com.androidsample.R;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String PACKAGE = MainActivity.class.getPackage().getName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateLibraryList();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void updateLibraryList() {
+        final ListView libraryListView = (ListView) findViewById(R.id.library_list);
+        final String[] libraryListString = getResources().getStringArray(R.array.library_names);
+        ArrayAdapter<String> libraryListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, libraryListString);
+
+
+        libraryListView.setAdapter(libraryListAdapter);
+
+        libraryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final ListView listView = (ListView) parent;
+                String clickedItemName = (String) listView.getItemAtPosition(position);
+                String activityName = PACKAGE + "." + clickedItemName + "Activity";
+                try {
+                    Intent intent = new Intent(getApplicationContext(), Class.forName(activityName));
+                    startActivity(intent);
+                } catch (ClassNotFoundException e) {
+                    Log.e(TAG, "Activity Not Found :" +activityName);
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
