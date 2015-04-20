8002fccb4f30caadaea96c67b3bc713d0ebb050f
muran001 <macmuran001@gmail.com>
2015-04-20 23:38:59 +0900

    add TimberActivity

---
+++ app/src/main/java/com/muran001/anroid_library_sample/TimberActivity.java
@@ -0,0 +1,79 @@
+package com.muran001.anroid_library_sample;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import android_sample.muran001.com.androidsample.BuildConfig;
+import android_sample.muran001.com.androidsample.R;
+import timber.log.Timber;
+
+public class TimberActivity extends BaseLibraryActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timber);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+
+        setClickListener((Button) findViewById(R.id.firstButton));
+        setClickListener((Button) findViewById(R.id.secondButton));
+    }
+
+    private void setClickListener(final Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "button " + button.getText() + " click event is logged", Toast.LENGTH_SHORT).show();
+                Timber.tag(button.getText().toString());
+                Timber.d("button " + button.getText() + " is clicked");
+            }
+        });
+    }
+
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+            if (priority == Log.VERBOSE || priority == Log.DEBUG) {
+                return;
+            }
+
+            FakeCrashLibrary.log(priority, tag, message);
+
+            if (t != null) {
+                if (priority == Log.ERROR) {
+                    FakeCrashLibrary.logError(t);
+                } else if (priority == Log.WARN) {
+                    FakeCrashLibrary.logWarning(t);
+                }
+            }
+        }
+    }
+
+
+}
+
+class FakeCrashLibrary {
+    private FakeCrashLibrary() {
+        throw new AssertionError("No instances.");
+    }
+
+    public static void log(int priority, String tag, String message) {
+        Log.d(tag, message);
+    }
+
+    public static void logWarning(Throwable t) {
+        t.printStackTrace();
+    }
+
+    public static void logError(Throwable t) {
+        t.printStackTrace();
+    }
+}
